63e9176be020920dac6b3ea9fd5fc8aa6a02cde6
Joao Henrique <jh5777720@gmail.com>
2025-05-10 10:30:56 -0300

    Delete atividades 3 directory

--- atividades 3/074 maior e menor valor em tupla.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-valores = tuple(randint(0,10) for c in range (5)) #coloca 5 valores aleatorios na tupla
-organizado = sorted(valores) #organiza em ordem crescente
-print(f'os valores são: {valores}.\n o maior é {organizado[-1]} e o menor {organizado[0]}')
--- atividades 3/080 lista ordenada sem repetiçoes.py
+++
@@ -1,8 +0,0 @@
-numeros = []
-for c in range (0, 5):
-    valores = int(input('digite um numero: '))
-    for itens in valores:
-        if valores < numeros:
-            numeros.insert(itens-1,valores)
-        else:
-            numeros.append(itens,valores)
