67ed3656886ebd7b39ff2f6a13f56683642ba1f3
Joao Henrique <jh5777720@gmail.com>
2025-05-12 21:01:23 -0300

    atualizaçao do exercicio 021
    recentemente mudei pro vs code e o editor não estava achando as musicas, a atualização foi: atualizar o endereço da musica pro vs code conseguir executar o código

--- atividades 1/021 reprodutor de musicas com escolhas.py
+++ atividades 1/021 reprodutor de musicas com escolhas.py
@@ -4,11 +4,11 @@ escolha = (str(input('qual vc prefere? ir para a selva? ou bater nas portas para
            .strip().lower().replace('céu', 'ceu'))
 
 if escolha == 'selva':
-    pygame.mixer.music.load ('jungle.mp3')
+    pygame.mixer.music.load ('atividades 1/jungle.mp3')
     print('bem vindo a selva baby')
 
 elif escolha  == 'ceu':
-    pygame.mixer.music.load ('heaven.mp3')
+    pygame.mixer.music.load ('atividades 1/heaven.mp3')
     print('batamos nas portas do céu')
 
 else:
