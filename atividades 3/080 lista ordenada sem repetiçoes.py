a7b3cdd38deee5af2c3414d97dd1ec18883e45cf
Joao Henrique <jh5777720@gmail.com>
2025-05-26 20:53:38 -0300

    exercicio completo com a ajuda do guanabara

--- atividades 3/080 lista ordenada sem repetiçoes.py
+++ atividades 3/080 lista ordenada sem repetiçoes.py
@@ -1,8 +1,18 @@
-numeros = []
-for c in range (0, 5):
-    valores = int(input('digite um numero: '))
-    for itens in valores:
-        if valores < numeros:
-            numeros.insert(itens-1,valores)
-        else:
-            numeros.append(itens,valores)
+lista = []
+pos = 0
+
+for cont in range (0,5):
+    valor = int(input('digite um valor: '))
+    if valor in lista:
+        print('digite um novo valor, esse já existe')
+    elif cont == 0 or valor > lista[-1]:
+        lista.append(valor)
+        print('valor adicionado na ultima posição')
+    else:
+        while True:
+            if valor < lista[pos]:
+                lista.insert(pos, valor)
+                print(f'valor adicionado na posição: {pos}')
+                break
+            pos += 1
+print(f'a lista ordenada fica: {lista}')
