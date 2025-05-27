53657f0e0d0581dfca93097ed088a3aba72d7f20
Joao Henrique <jh5777720@gmail.com>
2025-05-27 20:33:30 -0300

    Update 081 extraindo dados de uma lista.py
    correções  de bugs e adições de comentários explicando cada linha

--- atividades 3/081 extraindo dados de uma lista.py
+++ atividades 3/081 extraindo dados de uma lista.py
@@ -1,24 +1,25 @@
 lista = []
-cont = cinco = 0
+cont = cinco = 0  #variaveis
 escolha = 'S'
-while escolha == 'S':
-    valor = int(input('digite um valor:'))
-    escolha = input('você quer continuar? [S/N]').upper()
-    if valor == '5':
-        cinco += 1
-    if escolha == 'nN':
-        break
-    elif escolha not in 'SsNn':
-        print('nao entendi, responda apenas com S ou N')
-        while escolha2 != 'SsNn':
-            escolha2 = input('quer continuar? [S/N]')
-            
-    cont += 1
-listaNova = sorted(lista, reverse = True)
-print(f'foram digitados {cont} numeros.'
-      'a lista organizada de forma decrescente ficoiu:{listaNova}')
 
-if '5' in lista:
-    print('ah, ja ia esquecendo; o numero 5 está na lista, e foi digitado {cincos} vezes')
-else:
+while escolha == 'S': #enquanto escolha for igual a "S" o loop fica rodando
+    valor = int(input('digite um valor: ')) #input do usuario
+    escolha = input('você quer continuar? [S/N]: ').upper() #validaçao pro loop continuar rodando, caso seja n o loop para
+    if valor == 5: #se o valor digitado for igual a 5
+        cinco += 1 # a variavel cinco soma 1
+    if escolha == 'nN': #se a escolha for "N" o loop para
+        break #isso para o loop
+    while escolha not in ['S','N']: #caso o valor da escolha seja diferente de "S ou N" o loop vai ficar rodando
+        escolha = input('nao entendi, quer continuar? [S/N]').upper()
+
+    cont += 1 #cada vez q o loop girar o contador soma 1 pra mostrar no final quantos numeros foram adicionados na lista
+    lista += [valor]#lista recebe o valor digitado pelo usuario
+listaNova = sorted(lista, reverse = True) #ao final do loop ele organiza a lista em ordem decrescente
+
+print(f'foram digitados {cont} numeros. \n'
+      f'a lista organizada de forma decrescente ficou:{listaNova}') #mostra quantos numeros foram inseridos e a ordem
+
+if 5 in lista: #se o "5" estiver na lista, ele fala quantas vezes aparece
+    print(f'ah, ja ia esquecendo; o numero 5 está na lista, e foi digitado {cinco} vezes')
+else: #caso contrario mostra uma mensagem de que nao achou
     print('ah, ja ia esquecendo; cinco não está na lista')
