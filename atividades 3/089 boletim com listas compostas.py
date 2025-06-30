937afb719a00c76ab884002108e966a42de7b815
Joao Henrique <jh5777720@gmail.com>
2025-06-30 19:23:35 -0300

    Update 089 boletim com listas compostas.py
    arrumei o print do boletim, coloquei as pesquisas de alunos separadamente e legendei tudo

--- atividades 3/089 boletim com listas compostas.py
+++ atividades 3/089 boletim com listas compostas.py
@@ -1,26 +1,39 @@
+#listas usadas
 boletim = []
 aluno = []
-notas = []
-
-while True:
+notas1 = []
+notas2 = []
+nota = []
+check = 0
+while True: #enquanto for verdadeiro
     aluno.append(input('Digite o nome do aluno: '))
-    notas.append(input('Digite a primeira nota: '))
-    notas.append(input('digite a segunda nota: '))
+    notas1 = input('Digite a primeira nota: ')
+    notas2 = input('digite a segunda nota: ')
     escolha = input('deseja parar? [S/N]: ').upper()
 
-    aluno.append(notas[:])
-    boletim.append(aluno[:])
-  
+    nota.append(notas1[:]) #a lista nota recebe notas1
+    nota.append(notas2[:]) #nota recebe notas2
+    aluno.append(nota) #aluno recebe nota
+    boletim.append(aluno[:]) #boletim recebe aluno, contem nome e as duas notas
+    #todas essas listas ficam vazias dnv
+    nota = []
+    notas1 = []
+    notas2 = []
+    aluno = []
 
-    if escolha == 'S':
-        break
+    if escolha == 'S': #se escolha for 's'
+        break #tudo para
     
-    else:
-        while escolha not in ['S','N']:
+    else: #se nao
+        while escolha not in ['S','N']: # enquanto escolhas nao for "s" ou "n"
             escolha = input('nao entendi, digite novamente: [S/N]').upper()
 
 print('=-' * 30)
 print(f'n°   {"NOME":<10}      {"NOTAS":>20}')
-for c, alunos in enumerate(boletim):
- print(f'{c} {alunos[c]:<20} {alunos[0][0]:>20}')
- print('=-' * 30)
+for c, alunos in enumerate(boletim): #vai passar item por item na lista, no caso nome e nota até acabar tudo
+    print(f'{c} {alunos[0]:<20} {str(alunos[1]):>20}')
+print('=-' * 30)
+
+while check != '999': #verdadeiro enquanto for 999
+    check = int(input(f'digite o numero do aluno para ve-lo ou 999 pra encerrar: '))
+    print(f'{boletim[check]}') #procura o item na lista na posição q o usuario decidiu
