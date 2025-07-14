128f075d7a52402266f7bc75b632f0848a4ebbcc
Joao Henrique <jh5777720@gmail.com>
2025-07-14 09:22:01 -0300

    89/3 correção do validador
    agora, caso o usuário digite se quer continuar ou não, aparece uma mensagem de erro e o script não para

--- atividades 3/089 boletim com listas compostas.py
+++ atividades 3/089 boletim com listas compostas.py
@@ -18,8 +18,20 @@ while True: #enquanto for verdadeiro
         break #tudo para
     
     else: #se nao
-        while escolha not in ['S','N']: # enquanto escolhas nao for "s" ou "n"
-            escolha = input('nao entendi, digite novamente: [S/N]').upper()
+            while chance not in ['S','N']: # enquanto escolhas nao for "s" ou "n"
+                chance = input('nao entendi, digite novamente: [S/N]').upper().strip()
+                
+                if chance == 'S' or 'N': #se escolha for 'sim' o loop para
+                    chance = ()
+                    escolha = ()
+                    break
+                
+                else: # se não, as variaveis chance e escolha sao limpas pra receber novos valores
+                    chance = ()
+                    escolha = ()
+                
+        if chance == 'S': #se o usuario escolher sair do loop na resposta de erro, o codigo para pra gerar o boletim
+            break
 
 print('=-' * 30)
 print(f'n°   {"NOME":<10}      {"NOTAS":>20}')
