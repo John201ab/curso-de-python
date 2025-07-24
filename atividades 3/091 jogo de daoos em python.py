5fec81897ba4dede25e66c15fe5adeb861a03959
Joao Henrique <jh5777720@gmail.com>
2025-07-24 08:00:07 -0300

    Update 091 jogo de daoos em python.py
    to aprendendo a usar lambda, mas ta ai kkkkkk

--- atividades 3/091 jogo de daoos em python.py
+++ atividades 3/091 jogo de daoos em python.py
@@ -5,15 +5,12 @@ print('valores sorteados: ')
 for c in range (1,5):
     jogador['pos'] = c
     jogador['num'] = randint(0,9)
-
+    placar.append(jogador.copy())
     print(f'o jogador {c} tirou {jogador["num"]}')
-    if c == 1:
-        placar.append(jogador.copy())
-    else:
-        for p in placar:
-            if jogador['num'] <= placar[p]['num']:
-                placar.append(jogador.copy())
-            else:
-                placar.insert[-1](jpgador.copy())
-for p in placar:
-    print(p.items())
+ordenado = sorted(placar, key=lambda p: p['num'], reverse=True)
+
+print('=' * 50)
+for p in ordenado:
+    print(f'o jogador {p["pos"]} jogador tirou {p["num"]} pontos')
+
+print('=' * 50)
