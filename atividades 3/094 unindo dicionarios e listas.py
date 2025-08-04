30e02fc54e9fa2b19863f16e7da380ef8f4a88dc
Joao Henrique <jh5777720@gmail.com>
2025-08-04 11:53:51 -0300

    Create 094 unindo dicionarios e listas.py
    v1.0 cadastro de usuários com dicionários

---
+++ atividades 3/094 unindo dicionarios e listas.py
@@ -0,0 +1,23 @@
+usuarios = list()
+pessoas = dict()
+loop = mulheres = acima = idades_totais = 0
+
+while True:
+    pessoas["nome"] = str(input('Digite seu nome: '))
+    pessoas["sexo"] = str(input('Digite seu sexo [H/M]: ')).strip().upper()[0]
+    pessoas["idade"] = int(input('Digite sua idade: '))
+    escolha = input('Deseja continuar? [S/N]').strip().upper()[0]
+
+    usuarios.append(pessoas.copy())
+    loop += 1
+    idades_totais += pessoas["idade"]
+    if pessoas["sexo"] == "M":
+        mulheres += 1
+    if escolha == 'N':
+        break
+ 
+
+media = idades_totais / loop
+
+for k, c in enumerate(usuarios[k]["idade"]):
+    print(k)
