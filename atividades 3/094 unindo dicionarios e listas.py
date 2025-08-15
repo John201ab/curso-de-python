7c4b35b1c659ecffede27bd3ac68aa220042af4d
Joao Henrique <jh5777720@gmail.com>
2025-08-15 08:13:17 -0300

    Update 094 unindo dicionarios e listas.py
    alteração de formatação

--- atividades 3/094 unindo dicionarios e listas.py
+++ atividades 3/094 unindo dicionarios e listas.py
@@ -5,31 +5,28 @@ loop = numulheres= acima = idades_totais = v = 0
 
 while True:
     pessoas["nome"] = str(input('Digite seu nome: '))
-    pessoas["sexo"] = str(input('Digite seu sexo [H/M]: ')).strip().upper()[0]
+    pessoas["sexo"] = str(input('Digite seu sexo [F/M]: ')).strip().upper()[0]
     pessoas["idade"] = int(input('Digite sua idade: '))
     escolha = input('Deseja continuar? [S/N]').strip().upper()[0]
 
     usuarios.append(pessoas.copy())
     loop += 1
     idades_totais += pessoas["idade"]
-    if pessoas["sexo"] == "M":
-        numulheres += 1
+    if pessoas["sexo"] == "F":
         mulheres.append(pessoas.copy())
         
     if escolha == 'N':
         break
  
-
 media = idades_totais / loop
-
 
 print(f'o grupo tem: {loop} pessoas')
 print(f' a média de idade do grupo é: {media} anos')
 print(f'as mulheres cadastradas foram: ')
 for lopp, mulher in enumerate(mulheres):
-    print(mulher["nome"])
+    print(mulher["nome"], end=' ')
 print("lista das pessoas com idade acima da média: ")
 for voltas, velhos in enumerate(usuarios):
     if velhos["idade"] >= media:
-        print(f"nome:{velhos["nome"]}")
+        print(f"dados: {velhos} \n")
     
