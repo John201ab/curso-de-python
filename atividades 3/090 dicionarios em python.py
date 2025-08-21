2da2465f66f27d4d515648e9e8d0d63e4ba71843
Joao Henrique <jh5777720@gmail.com>
2025-08-21 16:17:56 -0300

    Update 090 dicionarios em python.py
    explicação de cada comando

--- atividades 3/090 dicionarios em python.py
+++ atividades 3/090 dicionarios em python.py
@@ -1,14 +1,12 @@
-boletim = list()
-pessoa = dict()
-pessoa['nome'] = input('digite o seu nome: ')
-pessoa['media'] = float(input('digite sua média'))
-boletim.append(pessoa.copy())
-if boletim[0]['media'] >= 5:
-    sit  = 'aprovado'
-else:
-    sit = 'reprovado'
+pessoa = dict()  #declaro "pessoa" como um dicionario
+pessoa['nome'] = str(input('digite o seu nome: ')) # crio a chave "nome" e peço pra preencher
+pessoa['media'] = float(input('digite sua média: ')) # crio a chave "média" e peço pra preencher
+if pessoa['media'] >= 5:  # se o valor de "média" for maior que 5
+    pessoa["situacao"]  = 'aprovado'  # crio a chave "situação" e preencho com "aprovado"
+else:  #se não
+    pessoa["situacao"]  = 'reprovado' ### crio a chave "situação" e preencho com "reprovado"
 
-for b in boletim:
-    print(f'o nome é: {b["nome"]}')
-    print(f'sua média é: {b["media"]}')
-    print(f'você foi: {sit}')
+print('=+' * 30)
+for key, value in pessoa.items():  ## faz voltas passando passando por cada item do dicionario
+    print(f'{key} é igual a {value}')
+   
