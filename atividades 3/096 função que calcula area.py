1a64bf857be05706005c00f369f98daa5135c415
Joao Henrique <jh5777720@gmail.com>
2025-08-28 15:57:32 -0300

    Create 096 função que calcula area.py
    exercício 96 completo e funcionando, só falta documentar as linhas

---
+++ atividades 3/096 função que calcula area.py
@@ -0,0 +1,8 @@
+def area(a,b):
+    total = a * b
+    print(total)
+altura = int(input('digite a altura do seu terreno: '))
+
+largura = int(input('digite a largura do seu terreno: '))
+
+area(altura, largura)
