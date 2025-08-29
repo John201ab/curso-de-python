fb60425bc24704ed89e3071fc2d79e0d7a03a43d
Joao Henrique <jh5777720@gmail.com>
2025-08-29 11:27:58 -0300

    Update 098 Função de contador.py

--- atividades 3/098 Função de contador.py
+++ atividades 3/098 Função de contador.py
@@ -1,7 +1,10 @@
 from time import sleep
 
-def contador():
-    
+def contador(a, b, c):
+    for numero in range(a + 1, b + 1, c):
+        print( numero, end= " ")
+        sleep(0.5)
+    print('Fim!')
 for contador in range(1, 11):
     print(contador, end=' ')
     sleep(0.5)
@@ -9,8 +12,16 @@ print('\n')
 for contador2 in range(10, 0, -1):
     print(contador2, end=' ')
     sleep(0.5)
+print("\n")
+inicio = int(input('Digite um numero inicial: '))
+fim = int(input('Digite o numero final: '))
+salt = int(input('Pulando de quanto em quanto? '))
+if salt == 0:
+    salt = 1
 
-inicio = input('Digite um numero inicial: ')
-fim = input('Digite o numero final: ')
-salt = input('Pulando de quanto em quanto? ')
+if fim < inicio:
+    salt = -abs(salt)
+    inicio = inicio - 1
+    fim = fim - 1
 
+contador(inicio, fim, salt)
