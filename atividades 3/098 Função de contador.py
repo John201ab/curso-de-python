c9652d4965ba8a3547a8fe6f32eb39751154c511
Joao Henrique <jh5777720@gmail.com>
2025-09-10 15:32:18 -0300

    Update 098 Função de contador.py
    correções exercício 98

--- atividades 3/098 Função de contador.py
+++ atividades 3/098 Função de contador.py
@@ -2,25 +2,34 @@ from time import sleep
 # função que faz contagem de x até y pulando de z em z
 def contador(a, b, c):
     for numero in range(a + 1, b + 1, c):
-        print( numero, end= " ")
+        print( numero, end= " ", flush = True)
         sleep(0.5)
     print('Fim!')
-for contador in range(1, 11):
-    print(contador, end=' ')
+
+#contador de 0 a 10
+for numeros in range(1, 11):
+    print(numeros, end=' ', flush = True)
     sleep(0.5)
 print('\n')
-for contador2 in range(10, 0, -1):
-    print(contador2, end=' ')
+
+#contador de 10 a 0
+for numeros2 in range(10, 0, -2):
+    print(numeros2, end=' ', flush = True)
     sleep(0.5)
 print("\n")
-inicio = int(input('Digite um numero inicial: '))
-fim = int(input('Digite o numero final: '))
+
+#coleta de dados para contagem personalizada
+inicio = int(input('Digite um numero inicial:  '))
+fim = int(input('Digite o numero final:        '))
 salt = int(input('Pulando de quanto em quanto? '))
+
+#se o usuario digitar a passada 0 o programa a trasforma em 1
 if salt == 0:
     salt = 1
 
+ #configuração pra contagens negativas
 if fim < inicio:
-    salt = -abs(salt)
+    salt = -abs(salt) #- abs transforma o numero positivo em negativo
     inicio = inicio - 1
     fim = fim - 1
 
