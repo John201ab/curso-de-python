618bec02a79174bd84ba23fffb714bba1dd5cad1
Joao Henrique <jh5777720@gmail.com>
2026-06-15 15:54:26 -0300

    Exercício da ficha de games, preciso apenas tratar as saídas e colocar em uma tabela.

---
+++ atividades 4/conceitos_POO/020_Ficha_Gamer/Ficha_gamer.py
@@ -0,0 +1,15 @@
+
+class Ficha():
+    def __init__ (self, nome, nick):
+        self.nome = nome
+        self.nick = nick
+
+    def Add_game(self, jogos):
+        games = []
+        games.append(jogos)
+
+
+c1 = Ficha("joão", "Johnzera")
+
+c1.Add_game("Gow")
+
