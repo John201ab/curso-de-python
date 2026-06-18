d82049e86808cc187f163bf03c7d9f107c3c58a1
Joao Henrique <jh5777720@gmail.com>
2026-06-18 16:02:27 -0300

    ficha de informação concluida

--- atividades 4/conceitos_POO/020_Ficha_Gamer/Ficha_gamer.py
+++ atividades 4/conceitos_POO/020_Ficha_Gamer/Ficha_gamer.py
@@ -1,3 +1,10 @@
+from rich import print
+from rich.panel import Panel
+from rich.traceback import install
+
+install()
+
+games = []
 
 class Ficha():
     def __init__ (self, nome, nick):
@@ -5,11 +12,17 @@ class Ficha():
         self.nick = nick
 
     def Add_game(self, jogos):
-        games = []
         games.append(jogos)
 
+    def table(self):
+        placa = Panel(f"""nick: {self.nick}
+nome: {self.nome}
+jogos salvos: {games}""", title = (f"ficha de <{self.nick}>"), expand = False)
+        print(placa)
 
 c1 = Ficha("joão", "Johnzera")
 
 c1.Add_game("Gow")
+c1.Add_game("COD")
 
+c1.table()
